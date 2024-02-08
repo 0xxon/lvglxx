87a8505419a0a03699311581973967fe643fbe83
Johanna Amann <1538460+0xxon@users.noreply.github.com>
2024-02-08 15:56:59 +0000

    Initial import

---
+++ README.md
@@ -0,0 +1,3 @@
+# LvglXX - a header-only C++ wrapper for LVGL
+
+LvglXX is a header-only C++ wrapper for LVGL.
---
+++ src/lvglxx.hpp
@@ -0,0 +1,157 @@
+// Please see "LICENSE" file for copyright and license information.
+
+#pragma once
+
+#include <lvgl.h>
+
+#include <string>
+#include <vector>
+#include <functional>
+
+// Helper classes for callbacks
+template<class WrapperType>
+using ui_event_callback = std::function<void (WrapperType&, lv_event_t*)>;
+
+class UiEventDispatchBase {
+public:
+    virtual void Dispatch(lv_event_t*);
+    static void EventCallback(lv_event_t* e) {
+        UiEventDispatchBase* dp = static_cast<UiEventDispatchBase*>(lv_event_get_user_data(e));
+        dp->Dispatch(e);
+    }
+};
+
+template <class WrapperType>
+class UiEventDispatch : public UiEventDispatchBase {
+public:
+    UiEventDispatch(WrapperType& w, ui_event_callback<WrapperType> ev) : wrapper_(w), cb_(ev) {}
+    void Dispatch(lv_event_t* e) override { cb_(wrapper_, e); }
+private:
+    WrapperType& wrapper_;
+    ui_event_callback<WrapperType> cb_;
+};
+
+template <typename Child>
+class BaseWrapper {
+public:
+    BaseWrapper() = default;
+
+    Child& SetFlexFlow(lv_flex_flow_t flow = LV_FLEX_FLOW_ROW_WRAP) { lv_obj_set_flex_flow(obj_, flow); return static_cast<Child&>(*this); }
+    Child& SetFlexAlign(lv_flex_align_t main_place = LV_FLEX_ALIGN_START, lv_flex_align_t cross_place = LV_FLEX_ALIGN_CENTER, lv_flex_align_t track_cross_place = LV_FLEX_ALIGN_START) { lv_obj_set_flex_align(obj_, main_place, cross_place, track_cross_place); return static_cast<Child&>(*this);  }
+    Child& SetAlign(lv_align_t align) { lv_obj_set_align(obj_, align); return static_cast<Child&>(*this); }
+    Child& SetFlexGrow(int32_t grow = 1) { lv_obj_set_flex_grow(obj_, grow); return static_cast<Child&>(*this); }
+    Child& SetFlexInNewTrack() { lv_obj_add_flag(obj_, LV_OBJ_FLAG_FLEX_IN_NEW_TRACK); return static_cast<Child&>(*this); }
+    Child& FullWidth() { lv_obj_set_width(obj_, lv_pct(100)); return static_cast<Child&>(*this); }
+    Child& SetWidth(int32_t width = LV_SIZE_CONTENT) { lv_obj_set_width(obj_, width); return static_cast<Child&>(*this); }
+    Child& SetHeight(int32_t height = LV_SIZE_CONTENT) { lv_obj_set_height(obj_, height); return static_cast<Child&>(*this); }
+    Child& SetX(int32_t x) { lv_obj_set_x(obj_, x); return static_cast<Child&>(*this); }
+    Child& SetY(int32_t y) { lv_obj_set_y(obj_, y); return static_cast<Child&>(*this); }
+    Child& AddState(lv_state_t state) { lv_obj_add_state(obj_, state); return static_cast<Child&>(*this); }
+    Child& RemoveState(lv_state_t state) { lv_obj_remove_state(obj_, state); return static_cast<Child&>(*this); }
+    Child& RemoveStyleAll() { lv_obj_remove_style_all(obj_); return static_cast<Child&>(*this); }
+    Child& SetTextColor(lv_color_t color) { lv_obj_set_style_text_color(obj_, color, LV_PART_MAIN); return static_cast<Child&>(*this); }
+    Child& PadRow(int32_t pad) { lv_obj_set_style_pad_row(obj_, pad, LV_PART_MAIN); return static_cast<Child&>(*this); }
+    Child& PadColumn(int32_t pad) { lv_obj_set_style_pad_column(obj_, pad, LV_PART_MAIN); return static_cast<Child&>(*this); }
+    Child& SetObject(lv_obj_t* object) { obj_ = object; return static_cast<Child&>(*this); }
+    Child& AddCallback(ui_event_callback<Child> cb, lv_event_code_t filter) {
+        auto callback = new UiEventDispatch<Child>(static_cast<Child&>(*this), cb);
+        lv_obj_add_event_cb(obj_, UiEventDispatchBase::EventCallback, filter, callback);
+        return static_cast<Child&>(*this);
+    }
+    Child& AddCallback(std::function<void ()> cb, lv_event_code_t filter) {
+        AddCallback([=](Child&, lv_event_t*){cb();}, filter);
+        return static_cast<Child&>(*this);
+    }
+    //operator lv_obj_t*() { return obj_; }
+    lv_obj_t* GetObj() { return obj_; }
+
+protected:
+    lv_obj_t* obj_ = nullptr;
+};
+
+class ObjectWrapper : public BaseWrapper<ObjectWrapper>{
+public:
+    explicit ObjectWrapper(lv_obj_t* parent) { obj_ = lv_obj_create(parent); }
+};
+
+template <typename Child>
+class LabelWrapperBase : public BaseWrapper<Child> {
+public:
+    LabelWrapperBase() = default;
+    explicit LabelWrapperBase(lv_obj_t* parent) {
+        this->obj_ = lv_label_create(parent);
+    }
+
+    LabelWrapperBase(lv_obj_t* parent, std::string_view text) {
+        this->obj_ = lv_label_create(parent);
+        lv_label_set_text(this->obj_, text.data());
+    }
+
+    Child& SetText(std::string_view text) { lv_label_set_text(this->obj_, text.data()); return static_cast<Child&>(*this); }
+};
+
+class LabelWrapper : public LabelWrapperBase<LabelWrapper> {
+public:
+    LabelWrapper() = default;
+    explicit LabelWrapper(lv_obj_t* parent) : LabelWrapperBase(parent) {}
+    LabelWrapper(lv_obj_t* parent, std::string_view text) : LabelWrapperBase(parent, text) {}
+};
+
+class ButtonWrapper : public BaseWrapper<ButtonWrapper> {
+public:
+    ButtonWrapper() = default;
+    explicit ButtonWrapper(lv_obj_t* parent) {
+        obj_ = lv_button_create(parent);
+        thelabel = LabelWrapper(obj_);
+        thelabel.SetWidth().SetHeight().SetText(""); // .SetAlign(LV_ALIGN_CENTER);
+    }
+    ButtonWrapper(lv_obj_t* parent, std::string_view text) : ButtonWrapper(parent) {
+        SetText(text);
+    }
+
+    LabelWrapper& Label() { return thelabel; }
+    ButtonWrapper& SetText(std::string_view text) { thelabel.SetText(text); return *this; }
+
+private:
+    LabelWrapper thelabel;
+};
+
+class BarWrapper : public BaseWrapper<BarWrapper> {
+public:
+    BarWrapper() = default;
+    explicit BarWrapper(lv_obj_t* parent) {
+        obj_ = lv_bar_create(parent);
+        thelabel = LabelWrapper(obj_);
+        thelabel.SetWidth().SetHeight().SetText("").SetAlign(LV_ALIGN_CENTER);
+    }
+
+    LabelWrapper& Label() { return thelabel; }
+
+    BarWrapper& SetValue(int32_t val) { lv_bar_set_value(obj_, val, LV_ANIM_OFF); return *this; }
+    BarWrapper& SetLabel(std::string_view text) { thelabel.SetText(text); return *this; }
+
+private:
+    LabelWrapper thelabel;
+};
+
+class DropdownWrapper : public BaseWrapper<DropdownWrapper> {
+public:
+    DropdownWrapper() = default;
+    explicit DropdownWrapper(lv_obj_t* parent) {
+        obj_ = lv_dropdown_create(parent);
+    }
+    DropdownWrapper(lv_obj_t* parent, std::vector<std::string> options) : DropdownWrapper(parent) {
+        SetOptions(options);
+    }
+
+    DropdownWrapper& SetOptions(std::vector<std::string> options) {
+        std::string out;
+        for ( auto& el : options )
+            out += el + "\n";
+        lv_dropdown_set_options(obj_, out.c_str());
+        return *this; 
+    }
+
+    DropdownWrapper& SetSelected(uint32_t s) { lv_dropdown_set_selected(obj_, s); return *this; }
+    uint32_t GetSelected() { return lv_dropdown_get_selected(obj_); }
+};
